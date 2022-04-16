3a8be3ebd35a54024dc17918203145f644d2e496
JeeyoungPark <jeeyoung0321@gmail.com>
2022-04-16 13:53:47 +0900

    add websocket server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+import { handle } from "express/lib/application";
 
 const app = express();
 
@@ -7,8 +10,14 @@ app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+// 사용자가 홈(/)으로 GET 요청을 보내면 home 템플릿을 제공
 app.get("/", (req, res) => res.render("home"));
+// 사용자가 홈이 아닌 다른 주소로 요청하면 홈으로 리다이렉트
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
