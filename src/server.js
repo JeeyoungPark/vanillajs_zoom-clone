9c45e28b2f13f2489277492b0e9945c9871c242c
JeeyoungPark <jeeyoung0321@gmail.com>
2022-04-17 16:17:56 +0900

    feat: connect server and send real-time message
    - when message seding event invoke, message is rendered as a list

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
-import { handle } from "express/lib/application";
 
 const app = express();
 
-app.set("view engine", "pug")
+app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-// 사용자가 홈(/)으로 GET 요청을 보내면 home 템플릿을 제공
+// NOTE 사용자가 홈(/)으로 GET 요청을 보내면 home 템플릿을 제공
 app.get("/", (req, res) => res.render("home"));
-// 사용자가 홈이 아닌 다른 주소로 요청하면 홈으로 리다이렉트
+// NOTE 사용자가 홈이 아닌 다른 주소로 요청하면 홈으로 리다이렉트
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
@@ -20,4 +19,18 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// NOTE fake database
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(`${message}`));
+
+    });
+});
+
 server.listen(3000, handleListen);
+
