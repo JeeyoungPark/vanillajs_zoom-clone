f8c95970338bf72ef231417d0daea8ffdd93fac0
JeeyoungPark <jeeyoung0321@gmail.com>
2022-04-17 19:24:22 +0900

    feat: send message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,12 +16,28 @@ const addMessage = (message) => {
     ul.appendChild(li);
 }
 
+const handleMessageSubmit = (event) => {
+    event.preventDefault();
+
+    const input = room.querySelector("input");
+    const value = input.value;
+
+    socket.emit("new_message", value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+
+    input.value = "";
+}
+
 const showRoom = () => {
     welcome.hidden = true;
     room.hidden = false;
 
     const h3 = room.querySelector("h3");
     h3.innerHTML = `Room: ${roomName}`;
+
+    const messageForm = room.querySelector("form");
+    messageForm.addEventListener("submit", handleMessageSubmit);
 }
 
 const handleRoomSubmit = (event) => {
@@ -42,4 +58,8 @@ socket.on("welcome", () => {
 
 socket.on("bye", () => {
     addMessage("someone left");
+});
+
+socket.on("send_message", (msg) => {
+    addMessage(msg);
 });
