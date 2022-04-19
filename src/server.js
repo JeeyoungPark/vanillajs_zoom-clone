67f212851cd1c0ae5ad0fc881e119e5088dd9384
JeeyoungPark <jeeyoung0321@gmail.com>
2022-04-19 23:47:30 +0900

    reset codes for preparing Video Chat

--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 import http from "http";
-// import SocketIO from "socket.io"
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import SocketIO from "socket.io"
 import express from "express";
 
 const app = express();
@@ -17,60 +15,7 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-// const wsServer = SocketIO(httpServer);
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true
-    }
-});
-
-instrument(wsServer, {
-    auth: false
-});
-
-const publicRooms = () => {
-    const { sids, rooms } = wsServer.sockets.adapter;
-
-    const publicRooms = [];
-
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    });
-
-    return publicRooms;
-}
-
-const countUser = (roomName) => {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-    socket["nick"] = "Anonymous";
-    socket.on("enter_room", (roomName, done) => {
-        done();
-        socket.join(roomName);
-        socket.to(roomName).emit("welcome", socket.nick, countUser(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nick, countUser(room) - 1));
-    });
-    
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("send_message", `${socket.nick}: ${msg}`);
-        done();
-    });
-    
-    socket.on("set_nick", (userNick) => (socket["nick"] = userNick));
-})
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
