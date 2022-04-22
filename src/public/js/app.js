f5d32ae1cbdb57df093566bb7e5380dfc9bbdb96
JeeyoungPark <jeeyoung0321@gmail.com>
2022-04-22 22:37:45 +0900

    feat: get camera stream
    - navigator.mediaDevices.getUserMedai API

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,48 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+const getMedia = async () => {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
+
+        myFace.srcObject = myStream;
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+getMedia();
+
+const handleMuteClick = () => {
+    if(!muted) {
+        muteBtn.innerText = "Unmuted";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+
+const handleCameraClick = () => {
+    if(!cameraOff) {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    } else {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+
