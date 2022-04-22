980c94200b97f0ad0708a0446e04d4478166bbe0
JeeyoungPark <jeeyoung0321@gmail.com>
2022-04-23 02:19:48 +0900

    feat: show peer's video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -126,19 +126,40 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
+    console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
 
     const answer = await myPeerConnection.createAnswer();
+
     myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
 });
 
-socket.on("answer", (answer, roomName) => {
+socket.on("answer", answer => {
+    console.log("received the answer");
     myPeerConnection.setRemoteDescription(answer);
-})
+});
+
+socket.on("ice", ice => {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);
+});
 
 // NOTE RTC Code
 const makeConnection = () => {
     myPeerConnection = new RTCPeerConnection();
-    console.log(myStream.getTracks());
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+const handleIce = (data) => {
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+}
+
+const handleAddStream = (data) => {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
 }
